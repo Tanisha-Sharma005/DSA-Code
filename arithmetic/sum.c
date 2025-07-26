70682a06a94666a726625140bb0b83f442cb850b
Tanisha-Sharma005 <tanishaasharma20@gmail.com>
2025-07-26 20:04:55 +0530

    Create sum.c

---
+++ arithmetic/sum.c
@@ -0,0 +1,9 @@
+// Program to calculate sum of two numbers
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    printf("Sum = %d\n", a + b);
+    return 0;
+}
