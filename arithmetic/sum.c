a4012fc5ac4130a036b7852e438c9645292955f9
Tanisha <tanishaasharma20@gmail.com>
2025-08-23 21:36:47 +0530

    Delete arithmetic directory

--- arithmetic/sum.c
+++
@@ -1,9 +0,0 @@
-// Program to calculate sum of two numbers
-#include <stdio.h>
-int main() {
-    int a, b;
-    printf("Enter two numbers: ");
-    scanf("%d%d", &a, &b);
-    printf("Sum = %d\n", a + b);
-    return 0;
-}
